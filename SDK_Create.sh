111704e974cde4eb5d7b5417660c7ee3f0d17b7b
Fil <filippo.visocchi@novasomindustries.com>
2020-08-21 16:39:16 +0200

    repo downloaded by default

--- SDK_Create.sh
+++ SDK_Create.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 DIRS="AutoRun Codeblocks DtbUserWorkArea FileSystems Logs Qt Utils-2020.09 XCompilers-2020.09 Bootloaders Deploy Doc-2020.09 ExternalFileSystems Kernels NOVAembed_Settings Packages-2020.09"
 
+if [ -d NOVAsdk2020.09 ]; then
+	echo "Dir NOVAsdk2020.09 is present. Remove it before continuing"
+	exit
+fi
 mkdir NOVAsdk2020.09
 ln -s NOVAsdk2020.09 NOVAsdk
 cd NOVAsdk
@@ -12,11 +16,11 @@ ln -s XCompilers-2020.09 XCompilers
 ln -s Doc-2020.09 Doc
 ln -s Packages-2020.09 Packages
 
-if ! [ -f ~/bin/repo ]; then
-	mkdir ~/bin
-	curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-	chmod a+x ~/bin/repo
-fi
+rm -rf ~/bin
+mkdir ~/bin
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+git config --global color.ui false 
 
 ~/bin/repo init -u https://github.com/NovasomIndustries/SDK_Manifest
 ~/bin/repo sync
