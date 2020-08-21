35523d34a073a8cb5c5affd0015cb07888baad4c
Fil <filippo.visocchi@novasomindustries.com>
2020-08-21 16:21:57 +0200

    Added script for SDK creation

---
+++ SDK_Create.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+DIRS="AutoRun Codeblocks DtbUserWorkArea FileSystems Logs Qt Utils-2020.09 XCompilers-2020.09 Bootloaders Deploy Doc-2020.09 ExternalFileSystems Kernels NOVAembed_Settings Packages-2020.09"
+
+mkdir NOVAsdk2020.09
+ln -s NOVAsdk2020.09 NOVAsdk
+cd NOVAsdk
+for i in ${DIRS}; do
+	mkdir ${i}
+done
+ln -s Utils-2020.09 Utils
+ln -s XCompilers-2020.09 XCompilers
+ln -s Doc-2020.09 Doc
+ln -s Packages-2020.09
+
+if ! [ -f ~/bin/repo ]; then
+	mkdir ~/bin
+	curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+	chmod a+x ~/bin/repo
+fi
+
+~/bin/repo init -u https://github.com/NovasomIndustries/SDK_Manifest
+~/bin/repo sync
+
+
+
